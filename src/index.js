3a0a6f589c2cafa278a07a850183c870ea6450da
Hannah Zuber <hgomberg@gmail.com>
2019-09-01 11:13:23 +0300

    whoops

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-console.log(process.env.REACT_APP_API_KEY);
-
 ReactDOM.render(
   <BrowserRouter>
     <App />
