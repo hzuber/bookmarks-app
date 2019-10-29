31164f673b705799d8efbf93686c4b8a7b9ea2bf
Hannah Zuber <hgomberg@gmail.com>
2019-10-29 09:23:10 +0100

    j

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://arcane-inlet-40781.herokuapp.com/`,
+  API_ENDPOINT: `https://arcane-everglades-98233.herokuapp.com/`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
